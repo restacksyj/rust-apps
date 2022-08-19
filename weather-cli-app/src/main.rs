b36705a24d5fb9dfef0c5bf972c38217e958c49f
restacksyj <theandroiddeveloper.yj@gmail.com>
2022-08-20 05:04:24 +0530

    added same input functionality

--- weather-cli-app/src/main.rs
+++ weather-cli-app/src/main.rs
@@ -16,15 +16,20 @@
 mod structs;
 use structs::{GeoLocation, CurrentWeather, WeatherRoot};
 use serde_json;
+use std::io;
 
 extern crate dotenv;
 
 #[macro_use]
 extern crate dotenv_codegen;
 
-fn take_input() {
-    let buffer = String::new();
-    
+fn take_input() -> String {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    match stdin.read_line(&mut buffer) {
+        Ok(_) => buffer.to_string().strip_suffix('\n').unwrap().to_string(),
+        Err(e) => e.to_string(),
+    }
 }
 
 async fn get_coordinates(city: &str) -> Result<Vec<GeoLocation>, reqwest::Error> {
@@ -53,9 +58,14 @@ async fn get_weather(lat: f64, lon: f64) -> Result<CurrentWeather, reqwest::Erro
 
 #[tokio::main]
 async fn main() {
-    println!("Welcome to weather app");
+    println!("Welcome to the weather app");
     dotenv::from_filename(".env").ok();
-    let coordinates = match get_coordinates("pune").await {
+    println!("Enter your city");
+    let city = take_input();
+    if city.len() < 3 {
+        println!("How do you think I work, eh? \n More than 3 letters please");
+    } else {
+        let coordinates = match get_coordinates(&city).await {
         Ok(n) => n,
         Err(_e) => Vec::<GeoLocation>::with_capacity(0),
     };
@@ -66,8 +76,10 @@ async fn main() {
             Err(_) => panic!("Something went wrong"), //poor implementation don't know what else to do
         };
 
-        println!("The weather is {}", weather.temperature);
+        println!("The weather in {} is {}", city, weather.temperature);
     } else {
         println!("Sorry no city found for this name")
     }
+    }
+    
 }
--- weather-cli-app/src/structs.rs
+++ weather-cli-app/src/structs.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use serde_json;
 
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
